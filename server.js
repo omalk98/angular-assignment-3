d8fe3bb57e26db140245ff093c356aa308806a3a
Omar <77292466+omalk98@users.noreply.github.com>
2022-03-19 02:05:53 -0400

    server repair

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 const app = express();
 
-app.use(express.static('./dist/Asn3-omar_hussein'));
+app.use(express.static('./dist/asn3-omar_hussein'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Asn3-omar_hussein/'}),
+    res.sendFile('index.html', {root: 'dist/asn3-omar_hussein/'}),
 );
 
 app.listen(process.env.PORT || 8080);
